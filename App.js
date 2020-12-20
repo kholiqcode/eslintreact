08144ae308624203ac55db4767ec32dcb66f7630
Kholiq <kholiqcode@users.noreply.github.com>
2020-12-21 06:43:00 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, Text } from 'react-native';
+
+const App = () => (
+  <>
+    <StatusBar barStyle="dark-content" />
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  </>
+);
+
+export default App;
